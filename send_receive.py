ac87dc3d2fc5c80929e2db29202f2f4d21ad4122
EmiNiemand <Szymon_PL@live.com>
2022-05-18 13:29:05 +0200

    update

---
+++ send_receive.py
@@ -0,0 +1,52 @@
+import socket
+import pyaudio
+
+def send(audio_setup: [int, int, int, int], ip_port: [str, int]):
+    p = pyaudio.PyAudio()
+    chunk, input_format, chanel_number, rate = audio_setup
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect(ip_port)
+
+        try:
+            stream = p.open(
+                format=input_format,
+                channels=chanel_number,
+                rate=rate,
+                frames_per_buffer=chunk,
+                input=True)
+
+            while True:
+                data = stream.read(chunk)
+                client_socket.sendall(data)
+        finally:
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
+
+
+def receive(audio_setup: [int, int, int, int], port):
+    p = pyaudio.PyAudio()
+    chunk, input_format, chanel_number, rate = audio_setup
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind(("", port))
+        server_socket.listen()
+        conn, addr = server_socket.accept()
+        print(f"Connected with: {addr}")
+
+        try:
+            stream = p.open(
+                format=input_format,
+                channels=chanel_number,
+                rate=rate,
+                frames_per_buffer=chunk,
+                output=True)
+            while True:
+                data = conn.recv(1024)
+                stream.write(data)
+        finally:
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
+
