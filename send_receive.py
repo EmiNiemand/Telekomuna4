ce28e3cf8a92b01d8f749ab2d9ffcaaee85db8e0
EmiNiemand <Szymon_PL@live.com>
2022-05-18 14:47:16 +0200

    update all

--- send_receive.py
+++ send_receive.py
@@ -1,50 +1,51 @@
 import socket
 import pyaudio
 
-def send(audio_setup: [int, int, int, int], ip_port: [str, int]):
+def send(audio_setup: [], ip_port: [str, int]):
     p = pyaudio.PyAudio()
-    chunk, input_format, chanel_number, rate = audio_setup
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect(ip_port)
 
         try:
             stream = p.open(
-                format=input_format,
-                channels=chanel_number,
-                rate=rate,
-                frames_per_buffer=chunk,
+                format=audio_setup[1],
+                channels=audio_setup[2],
+                rate=audio_setup[3],
+                frames_per_buffer=audio_setup[0],
                 input=True)
 
             while True:
-                data = stream.read(chunk)
+                data = stream.read(audio_setup[0])
                 client_socket.sendall(data)
+
         finally:
             stream.stop_stream()
             stream.close()
             p.terminate()
 
 
-def receive(audio_setup: [int, int, int, int], port):
+def receive(audio_setup: [], port):
     p = pyaudio.PyAudio()
-    chunk, input_format, chanel_number, rate = audio_setup
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind(("", port))
         server_socket.listen()
         conn, addr = server_socket.accept()
-        print(f"Connected with: {addr}")
+        print(f"Połączono z: {addr}")
 
         try:
             stream = p.open(
-                format=input_format,
-                channels=chanel_number,
-                rate=rate,
-                frames_per_buffer=chunk,
-                output=True)
+                format=audio_setup[1],
+                channels=audio_setup[2],
+                rate=audio_setup[3],
+                frames_per_buffer=audio_setup[0],
+                input=True)
+
             while True:
                 data = conn.recv(1024)
                 stream.write(data)
+
         finally:
             stream.stop_stream()
             stream.close()
