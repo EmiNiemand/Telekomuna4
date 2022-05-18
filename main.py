e42c144daf506c5212fd68e88765f1f92bd5330f
EmiNiemand <Szymon_PL@live.com>
2022-05-18 14:59:33 +0200

    final update

--- main.py
+++ main.py
@@ -15,28 +15,23 @@ def main():
     begin_transmission(port, ip, audio_setup)
 
 def get_input_format():
-    audio_input_formats = [pyaudio.paInt8, pyaudio.paInt16, pyaudio.paInt24, pyaudio.paInt32]
+    audio_formats = [pyaudio.paInt8, pyaudio.paInt16, pyaudio.paInt24, pyaudio.paInt32]
     print("Podaj format wejściowy: ",
-          "(1) Int8\n",
-          "(2) Int16\n",
-          "(3) Int24\n",
-          "(4) Int32")
+          "1.Int8\n",
+          "2.Int16\n",
+          "3.Int24\n",
+          "4.Int32")
     input_format = int(input(""))
-    input_format = audio_input_formats[input_format - 1]
+    input_format = audio_formats[input_format - 1]
     return input_format
 
 
-def begin_transmission(port, ip, audio_setup: [int, int, int, int]):
+def begin_transmission(port, ip, audio_setup: []):
     sender_thread = threading.Thread(target=sr.send, args=(audio_setup, (ip, port)))
     receiver_thread = threading.Thread(target=sr.receive, args=(audio_setup, port))
 
-    try:
-        print("[Ctrl+C by przerwać]")
-        receiver_thread.start()
-        input("Naciśnij dowolny klawisz, aby rozpocząć połączenie")
-        sender_thread.start()
-    except KeyboardInterrupt:
-        input("Przerwano połączenie. Naciśnij dowolny klawisz, by kontynuować...")
+    receiver_thread.start()
+    sender_thread.start()
 
 
 if __name__ == '__main__':
